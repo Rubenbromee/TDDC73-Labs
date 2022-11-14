e25f076ab54991aa6491e265a845fbbdcaad98f7
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-11-14 21:38:02 +0100

    lab 2 done and finished

--- MyApp/src/App.tsx
+++ MyApp/src/App.tsx
@@ -10,29 +10,20 @@
 
 import * as R from 'ramda';
 
-import React, { useEffect, useState, type PropsWithChildren } from 'react';
+import React, {useState} from 'react';
 import {
-	Dimensions,
-	ImageBackground,
-	SafeAreaView,
-	ScrollView,
-	StatusBar,
-	StyleSheet,
-	Text,
-	useColorScheme,
-	View,
+    Dimensions,
+    Image,
+    ImageBackground,
+    KeyboardAvoidingView,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
 } from 'react-native';
-import { Image } from 'react-native';
 
-import {
-	Colors,
-	DebugInstructions,
-	Header,
-	LearnMoreLinks,
-	ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import { DropDown } from './DropDown';
-import { TextInputField } from './TextInputField';
+import {DropDown} from './DropDown';
+import {TextInputField} from './TextInputField';
 
 let backgroundImage = require('./img/background.jpeg');
 
@@ -41,138 +32,233 @@ const swedbank = require('./img/swedbank.png');
 const chipProperties = Image.resolveAssetSource(chip);
 
 const Test: React.FC = () => {
-	return (
-		<View>
-			<Text>BAJS</Text>
-		</View>
-	);
+    return (
+        <View>
+            <Text>BAJS</Text>
+        </View>
+    );
 };
 
 const months = [
-	{ label: '01', value: '01' },
-	{ label: '02', value: '02' },
-	{ label: '03', value: '03' },
-	{ label: '04', value: '04' },
-	{ label: '05', value: '05' },
-	{ label: '06', value: '06' },
-	{ label: '07', value: '07' },
-	{ label: '08', value: '08' },
-	{ label: '09', value: '09' },
-	{ label: '10', value: '10' },
-	{ label: '11', value: '11' },
-	{ label: '12', value: '12' },
-]
+    {label: '01', value: '01'},
+    {label: '02', value: '02'},
+    {label: '03', value: '03'},
+    {label: '04', value: '04'},
+    {label: '05', value: '05'},
+    {label: '06', value: '06'},
+    {label: '07', value: '07'},
+    {label: '08', value: '08'},
+    {label: '09', value: '09'},
+    {label: '10', value: '10'},
+    {label: '11', value: '11'},
+    {label: '12', value: '12'},
+];
 
 // Generate years 2022 to 1977 for the year selection
-const years = R.range(1977, 2023).reverse().map(num => {
-	return {
-		label: num.toString(), value: num.toString()
-	}
-});
+const years = R.range(1977, 2023)
+    .reverse()
+    .map(num => {
+        return {
+            label: num.toString(),
+            value: num.toString(),
+        };
+    });
 
-const width = Dimensions.get("screen").width;
-const height = Dimensions.get("screen").height;
+const width = Dimensions.get('screen').width;
+
+// Takes in whatever string, returns a sanitized version of that string only containing alpha numeric numbers
+function numberSanitizier(in_data: string): string {
+    return in_data.replace(/[^0-9.]+/, '');
+}
+
+// Format incoming cardNumber containing 0 to 16 numbers as XXXX SPACE XXXX SPACE XXXX SPACE XXXX
+function formatedCardNumber(cardNumber: string): string {
+    let formattedString = '';
+    for (let i = 0; i < cardNumber.length; i++) {
+        formattedString += cardNumber[i];
+        if ((i + 1) % 4 == 0) formattedString += ' ';
+    }
+    return formattedString;
+}
 
 const App = () => {
+    const [cardNumber, setCardNumber] = useState<string>('');
+    const [cardHolder, setCardHolder] = useState<string>('');
+    const [cvv, setCvv] = useState<string>('');
+    const [month, setMonth] = useState<string>('');
+    const [year, setYear] = useState<string>('');
+    const [backside, setBackside] = useState<boolean>(false);
 
+    /* We do these 2 with custom handlers since they behave differently than normal */
+    const onCardNumberChange = (newValue: string) => {
+        if (newValue.length == 17) return;
+        setCardNumber(numberSanitizier(newValue));
+    };
+    const onCvvChange = (newValue: string) => {
+        if (newValue.length == 4) return;
+        setCvv(numberSanitizier(newValue));
+    };
 
-	const [cardNumber, setCardNumber] = useState<string>();
-	const [cardHolder, setCardHolder] = useState<string>();
-	const [cvv, setCvv] = useState<string>();
-	const [month, setMonth] = useState<string>('');
-	const [year, setYear] = useState<string>('');
-	const [backside, setBackside] = useState<boolean>(false);
-
-	useEffect(() => {
-		console.log(height);
-	}, [])
-
-	return (
-		<SafeAreaView>
-			<View style={{ justifyContent: 'center', alignItems: 'center', paddingTop: 50 }}>
-				<ImageBackground source={backgroundImage} style={{ width: width * 0.8, height: 200 }} imageStyle={{ borderRadius: 12 }}>
-					{
-						!backside ?
-							<>
-								<View style={style.chip}>
-									<Image source={chip} />
-								</View>
-								<View style={style.swedbank}>
-									<Image source={swedbank} />
-								</View>
-								<Text style={style.cardNumber}> {cardNumber} </Text>
-								<Text style={style.cardHolder}> {cardHolder}</Text>
-								<Text style={style.expirationDate}>{month + '/' + year.substring(2, 4)}</Text>
-							</> :
-							<>
-								<View style={{ ...StyleSheet.absoluteFillObject, backgroundColor: 'black', width: width * 0.8, height: 40, top: '10%' }}></View>
-								<View style={{ ...StyleSheet.absoluteFillObject, backgroundColor: 'white', width: width * 0.72, height: 40, top: '40%', left: '5%', justifyContent: 'center', alignItems: 'flex-end' }}>
-									<Text style={{ marginRight: 5 }}>{cvv}</Text>
-								</View>
-							</>
-					}
-
-				</ImageBackground>
-			</View>
-			<View style={{ marginTop: 150 }}>
-				<View style={{ justifyContent: 'center', alignItems: 'center' }}>
-					<TextInputField label={'Card Number'} valueSetter={setCardNumber} width={350} topMargin={10} />
-					<TextInputField label={'Card Holder'} valueSetter={setCardHolder} width={350} topMargin={10} />
-				</View>
-				<View style={{ flexDirection: 'row', width: width, alignItems: 'center', justifyContent: 'center' }}>
-					<View style={{ marginHorizontal: 10 }}>
-						<DropDown listItems={months} valueSetter={setMonth} placeHolder='Month' />
-					</View>
-					<View style={{ marginHorizontal: 10 }}>
-						<DropDown listItems={years} valueSetter={setYear} placeHolder='Year' />
-					</View>
-					<View style={{ marginHorizontal: 10 }}>
-						<TextInputField label={'CVV'} valueSetter={setCvv} width={100} topMargin={4} onFocusFunction={setBackside} />
-					</View>
-				</View>
-			</View>
-		</SafeAreaView>
-	);
+    return (
+        <KeyboardAvoidingView behavior="position">
+            <SafeAreaView>
+                {/* Card starts here */}
+                <View
+                    style={{
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        paddingTop: 25,
+                    }}>
+                    <ImageBackground
+                        source={backgroundImage}
+                        style={{width: width * 0.8, height: 200}}
+                        imageStyle={{borderRadius: 12}}>
+                        {!backside ? (
+                            <>
+                                <View style={style.chip}>
+                                    <Image source={chip} />
+                                </View>
+                                <View style={style.swedbank}>
+                                    <Image source={swedbank} />
+                                </View>
+                                <Text style={style.cardNumber}>
+                                    {' '}
+                                    {formatedCardNumber(cardNumber)}{' '}
+                                </Text>
+                                <Text style={style.cardHolder}>
+                                    {' '}
+                                    {cardHolder}
+                                </Text>
+                                <Text style={style.expirationDate}>
+                                    {month + '/' + year.substring(2, 4)}
+                                </Text>
+                            </>
+                        ) : (
+                            <>
+                                <View
+                                    style={{
+                                        ...StyleSheet.absoluteFillObject,
+                                        backgroundColor: 'black',
+                                        width: width * 0.8,
+                                        height: 40,
+                                        top: '10%',
+                                    }}></View>
+                                <View
+                                    style={{
+                                        ...StyleSheet.absoluteFillObject,
+                                        backgroundColor: 'white',
+                                        width: width * 0.72,
+                                        height: 40,
+                                        top: '40%',
+                                        left: '5%',
+                                        justifyContent: 'center',
+                                        alignItems: 'flex-end',
+                                    }}>
+                                    <Text style={{marginRight: 5}}>{cvv}</Text>
+                                </View>
+                            </>
+                        )}
+                    </ImageBackground>
+                </View>
+                {/* Card ends here */}
+                <View style={{marginTop: 25}}>
+                    <View
+                        style={{
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+                        <TextInputField
+                            value={cardNumber}
+                            label={'Card Number'}
+                            setValue={onCardNumberChange}
+                            width={350}
+                            topMargin={10}
+                            onFocusFunction={() => setBackside(false)}
+                        />
+                        <TextInputField
+                            value={cardHolder}
+                            label={'Card Holder'}
+                            setValue={setCardHolder}
+                            width={350}
+                            topMargin={10}
+                            onFocusFunction={() => setBackside(false)}
+                        />
+                    </View>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            width: width,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        <View style={{marginHorizontal: 10}}>
+                            <DropDown
+                                listItems={months}
+                                valueSetter={setMonth}
+                                placeHolder="Month"
+                            />
+                        </View>
+                        <View style={{marginHorizontal: 10}}>
+                            <DropDown
+                                listItems={years}
+                                valueSetter={setYear}
+                                placeHolder="Year"
+                            />
+                        </View>
+                        <View style={{marginHorizontal: 10}}>
+                            <TextInputField
+                                value={cvv}
+                                label={'CVV'}
+                                setValue={onCvvChange}
+                                width={100}
+                                topMargin={4}
+                                onFocusFunction={() => setBackside(true)}
+                            />
+                        </View>
+                    </View>
+                </View>
+            </SafeAreaView>
+        </KeyboardAvoidingView>
+    );
 };
 
-
 const style = StyleSheet.create({
-	chip: {
-		...StyleSheet.absoluteFillObject,
-		transform: [{ scaleX: 0.60 }, { scaleY: 0.60 }],
-		left: '-20%',
-		top: '-15%',
-	},
-	swedbank: {
-		...StyleSheet.absoluteFillObject,
-		transform: [{ scaleX: 0.25 }, { scaleY: 0.25 }],
-		left: '12%',
-		top: '-50%'
-	},
-	cardNumber: {
-		...StyleSheet.absoluteFillObject,
-		fontSize: 20,
-		color: 'white',
-		fontWeight: 'bold',
-		left: '5%',
-		top: '45%'
-	},
-	cardHolder: {
-		...StyleSheet.absoluteFillObject,
-		fontSize: 20,
-		color: 'white',
-		fontWeight: 'bold',
-		left: '5%',
-		top: '80%'
-	},
-	expirationDate: {
-		...StyleSheet.absoluteFillObject,
-		fontSize: 20,
-		color: 'white',
-		fontWeight: 'bold',
-		left: '70%',
-		top: '80%'
-	}
+    chip: {
+        ...StyleSheet.absoluteFillObject,
+        transform: [{scaleX: 0.6}, {scaleY: 0.6}],
+        left: '-20%',
+        top: '-15%',
+    },
+    swedbank: {
+        ...StyleSheet.absoluteFillObject,
+        transform: [{scaleX: 0.25}, {scaleY: 0.25}],
+        left: '12%',
+        top: '-50%',
+    },
+    cardNumber: {
+        ...StyleSheet.absoluteFillObject,
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold',
+        left: '5%',
+        top: '45%',
+    },
+    cardHolder: {
+        ...StyleSheet.absoluteFillObject,
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold',
+        left: '5%',
+        top: '80%',
+    },
+    expirationDate: {
+        ...StyleSheet.absoluteFillObject,
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold',
+        left: '70%',
+        top: '80%',
+    },
 });
 
 export default App;
--- MyApp/src/TextInputField.tsx
+++ MyApp/src/TextInputField.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
-import { } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import {} from 'react-native';
+import {TextInput} from 'react-native-paper';
 
 type Props = {
-	label: string;
-	valueSetter: React.Dispatch<React.SetStateAction<string | undefined>>;
-	width: number,
-	topMargin: number,
-	onFocusFunction?: React.Dispatch<React.SetStateAction<boolean>>,
+    label: string;
+    value: string;
+    setValue:
+        | React.Dispatch<React.SetStateAction<string | undefined>>
+        | ((arg0: string) => void);
+    width: number;
+    topMargin: number;
+    onFocusFunction?: () => void;
 };
 
-export const TextInputField: React.FC<Props> = ({ label, valueSetter, width, topMargin, onFocusFunction }) => {
-	return (
-		<TextInput
-			label={label}
-			mode="outlined"
-			outlineStyle={{ borderRadius: 2, borderColor: '#b7dffd' }}
-			style={{ backgroundColor: 'white', marginTop: topMargin, marginBottom: 10, width: width, marginHorizontal: 0, padding: 0 }}
-			onChangeText={(text) => { valueSetter(text) }}
-			onFocus={onFocusFunction ? () => { onFocusFunction(true) } : () => { }}
-			onBlur={onFocusFunction ? () => { onFocusFunction(false) } : () => { }}
-		/>
-	);
+export const TextInputField: React.FC<Props> = ({
+    label,
+    value,
+    setValue,
+    width,
+    topMargin,
+    onFocusFunction,
+}) => {
+    return (
+        <TextInput
+            label={label}
+            value={value}
+            mode="outlined"
+            outlineStyle={{borderRadius: 2, borderColor: '#b7dffd'}}
+            style={{
+                backgroundColor: 'white',
+                marginTop: topMargin,
+                marginBottom: 10,
+                width: width,
+                marginHorizontal: 0,
+                padding: 0,
+            }}
+            onChangeText={text => {
+                setValue(text);
+            }}
+            onFocus={() => onFocusFunction && onFocusFunction()}
+        />
+    );
 };

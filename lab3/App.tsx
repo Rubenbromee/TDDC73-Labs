de010bac823edc4d8cf9a5fb8e9263b0b87329b3
kraken-dave-windows <davidstyrbjorn@gmail.com>
2022-11-22 12:43:19 +0100

    lab 3 done

--- lab3/App.tsx
+++ lab3/App.tsx
@@ -1,177 +1,241 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Button, Pressable, ScrollView, Text, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
-
+import {NavigationContainer} from '@react-navigation/native';
+import {
+    createNativeStackNavigator,
+    NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+import {
+    Pressable,
+    ScrollView,
+    Text,
+    TouchableOpacity,
+    useWindowDimensions,
+    View,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {useEffect, useState} from 'react';
+import {
+    ActivityIndicator,
+    MD3DarkTheme as DefaultTheme,
+    MD3Theme,
+} from 'react-native-paper';
+import {Provider as PaperProvider} from 'react-native-paper';
 
 type Repo = {
-	name: string;
-	description: string;
-	language: string;
-	stargazers_count: number;
-	created_at: string; // ISO-String
+    name: string;
+    description: string;
+    language: string;
+    stargazers_count: number;
+    created_at: string; // ISO-String
 };
 
 // Typing for navigation components
 type RootStackParamList = {
-	Overview: {
-		repos: Repo[];
-	};
-	Repository: {
-		repo: Repo;
-	};
+    Overview: {};
+    Repository: {
+        repo: Repo;
+    };
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-// Function for retrieveing a list of repo's
-function retrieveRepos(): Repo[] {
-	return [
-		{
-			created_at: '2022-01-01',
-			description: 'At vero  ',
-			stargazers_count: 1,
-			language: 'Java',
-			name: 'Java Project',
-		},
-		{
-			created_at: '2022-01-02',
-			description: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. ',
-			stargazers_count: 2,
-			language: 'Python',
-			name: 'Python Project',
-		},
-		{
-			created_at: '2022-01-03',
-			description: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. ',
-			stargazers_count: 3,
-			language: 'JavaScript',
-			name: 'JavasSript Project',
-		},
-		{
-			created_at: '2022-01-04',
-			description: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. ',
-			stargazers_count: 4,
-			language: 'C++',
-			name: 'C++ Project',
-		},
-		{
-			created_at: '2022-01-05',
-			description: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. ',
-			stargazers_count: 5,
-			language: 'C',
-			name: 'C Project',
-		},
-	];
+function getRequestURL(from_date_iso: string) {
+    return `https://api.github.com/search/repositories?q=language:rust&sort=stars&order=desc&created:>${from_date_iso}`;
 }
 
 // Function for sorting a given list of repo's
 function filterRepos(repos: Repo[]): Repo[] {
-	return repos;
+    return repos;
 }
 
 // Shorten text to a given number of characters and add ellipsis
 const shortenDiscriptionText = (text: string, length: number) => {
-	let shortText = text.substring(0, length);
-	return shortText.length >= length ? shortText.concat('', '...') : shortText;
-}
+    let shortText = text.substring(0, length);
+    return shortText.length >= length ? shortText.concat('', '...') : shortText;
+};
 
 // Component for listing repo's on click navigate to that repo
 const Overview: React.FC = () => {
-	const repos = retrieveRepos();
+    const {width, height} = useWindowDimensions();
 
-	return (
-		<ScrollView contentContainerStyle={{ alignItems: 'center' }}>
-			{repos.map((repo, idx) => {
-				return <RepoItem repo={repo} key={idx} />
-			})}
-		</ScrollView>
-	);
+    const {repos} = useGithubRepoData();
+    if (repos.length == 0) {
+        return (
+            <View
+                style={{
+                    width: width,
+                    height: height,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                <ActivityIndicator size="large" color="#00ff00" />
+            </View>
+        );
+    }
+
+    return (
+        <ScrollView contentContainerStyle={{alignItems: 'center'}}>
+            {repos.map((repo, idx) => {
+                return <RepoItem repo={repo} key={idx} />;
+            })}
+        </ScrollView>
+    );
 };
 
-type RepositoryProps = NativeStackScreenProps<RootStackParamList, 'Repository'>
+type RepositoryProps = NativeStackScreenProps<RootStackParamList, 'Repository'>;
 
 // Component for displaying detailed information about a given repo
-const Repository: React.FC<RepositoryProps> = ({ route, navigation }) => {
-	return (
-		<View>
-			<Text style={{ fontSize: 20, fontWeight: 'bold' }}>
-				{route.params.repo.name}
-			</Text>
-		</View>
-	);
+const Repository: React.FC<RepositoryProps> = ({route, navigation}) => {
+    return (
+        <View>
+            <Text
+                style={{
+                    textAlign: 'center',
+                    fontSize: 30,
+                    fontWeight: 'bold',
+                    color: 'black',
+                }}>
+                {route.params.repo.name}
+            </Text>
+            <View style={{paddingTop: 32}}>
+                <Text style={{color: 'black', textAlign: 'center'}}>
+                    {route.params.repo.description}
+                </Text>
+            </View>
+        </View>
+    );
 };
 
 type RepoItemProps = {
-	repo: Repo,
-}
+    repo: Repo;
+};
 
 // Component for displaying a repo in a list
-const RepoItem: React.FC<RepoItemProps> = ({ repo }) => {
-	const navigation = useNavigation();
-	const [buttonColor, setButtonColor] = useState('#24a0ed')
-	return (
-		<View style={{ backgroundColor: 'white', width: '90%', marginVertical: 5, borderRadius: 5, padding: 10 }}>
-			<Text style={{ fontSize: 20, fontWeight: 'bold' }}>{repo.name}</Text>
-			<View style={{ flexDirection: 'row', marginTop: 5 }}>
-				<Text style={{ fontWeight: 'bold' }}>
-					Created:{' '}
-				</Text>
-				<Text>
-					{repo.created_at}
-				</Text>
-			</View>
-			<Text style={{ fontSize: 15, fontWeight: 'bold', marginTop: 5 }}>Description</Text>
-			<Text>{shortenDiscriptionText(repo.description, 250)}</Text>
-			<View style={{ flexDirection: 'row', marginTop: 5 }}>
-				<Text style={{ fontWeight: 'bold' }}>Language: </Text>
-				<Text>{repo.language}</Text>
-			</View>
-			<View style={{ flexDirection: 'row', marginTop: 5 }}>
-				<Text style={{ fontWeight: 'bold' }}>
-					Stars:{' '}
-				</Text>
-				<Text>
-					{repo.stargazers_count}
-				</Text>
-				<View style={{ width: '85%', justifyContent: 'flex-end', flexDirection: 'row' }}>
-					<Pressable
-						style={{ backgroundColor: buttonColor, width: 100, height: 30, justifyContent: 'center', alignItems: 'center', borderRadius: 3 }}
-						onPress={() => navigation.navigate('Repository' as never, { repo: repo } as never)}
-						onPressIn={() => setButtonColor('#0a517e')}
-						onPressOut={() => setButtonColor('#24a0ed')}
-					>
-						<Text style={{ color: 'white' }}>Read more</Text>
-					</Pressable>
-				</View>
-			</View>
-		</View >
-	)
-}
+const RepoItem: React.FC<RepoItemProps> = ({repo}) => {
+    const navigation = useNavigation();
 
+    const handlePress = () => {
+        navigation.navigate('Repository' as never, {repo: repo} as never);
+    };
 
+    return (
+        <TouchableOpacity onPress={handlePress} style={{width: '80%'}}>
+            <View
+                style={{
+                    backgroundColor: 'white',
+                    marginVertical: 5,
+                    borderRadius: 5,
+                    padding: 10,
+                }}>
+                <Text
+                    style={{fontSize: 20, fontWeight: 'bold', color: 'black'}}>
+                    {repo.name}
+                </Text>
+                <View style={{flexDirection: 'row', marginTop: 5}}>
+                    <Text style={{fontWeight: 'bold', color: 'black'}}>
+                        Created:{' '}
+                    </Text>
+                    <Text style={{color: 'black'}}>
+                        {new Date(repo.created_at).toDateString()}
+                    </Text>
+                </View>
+                <Text style={{fontSize: 15, fontWeight: 'bold', marginTop: 5}}>
+                    Description
+                </Text>
+                <Text style={{color: 'black'}}>
+                    {shortenDiscriptionText(repo.description, 25)}
+                </Text>
+                <View style={{flexDirection: 'row', marginTop: 5}}>
+                    <Text style={{fontWeight: 'bold', color: 'black'}}>
+                        Language:{' '}
+                    </Text>
+                    <Text style={{color: 'black'}}>{repo.language}</Text>
+                </View>
+                <View style={{flexDirection: 'row', marginTop: 5}}>
+                    <Text
+                        style={{
+                            fontWeight: 'bold',
+                            color: 'black',
+                        }}>
+                        Stars:{' '}
+                    </Text>
+                    <Text style={{color: 'black'}}>
+                        {repo.stargazers_count}
+                    </Text>
+
+                    {/* <Pressable
+                    style={{
+                        backgroundColor: buttonColor,
+                        width: 100,
+                        height: 30,
+                        alignItems: 'center',
+                        borderRadius: 3,
+                        justifyContent: 'center',
+                    }}
+                    onPress={() =>
+                        navigation.navigate(
+                            'Repository' as never,
+                            {repo: repo} as never,
+                        )
+                    }
+                    onPressIn={() => setButtonColor('#0a517e')}
+                    onPressOut={() => setButtonColor('#24a0ed')}>
+                    <Text style={{color: 'white'}}>Read more</Text>
+                </Pressable> */}
+                </View>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+const useGithubRepoData = () => {
+    const [repos, setRepos] = useState<Repo[]>([]);
+
+    useEffect(() => {
+        // Make request
+        fetch(getRequestURL('2022-10-01'))
+            .then(value => value.json())
+            .then(json => {
+                setRepos(
+                    json.items.map((repoEntry: any) => {
+                        return {
+                            created_at: repoEntry.created_at,
+                            description: repoEntry.description,
+                            language: repoEntry.language,
+                            name: repoEntry.name,
+                            stargazers_count: repoEntry.stargazers_count,
+                        };
+                    }),
+                );
+            });
+    }, []);
+
+    return {repos};
+};
+
+const theme: MD3Theme = {
+    ...DefaultTheme,
+};
 
 const App = () => {
-	// Get list of unfiltered repos
-
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen
-					name="Overview"
-					component={Overview}
-					options={{ title: 'Select a trending repo!' }}
-				/>
-				<Stack.Screen
-					options={{ title: 'This is a repo' }}
-					name="Repository"
-					component={Repository}
-				/>
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+    return (
+        <PaperProvider theme={theme}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Overview"
+                        component={Overview}
+                        options={{title: 'Select a trending repo!'}}
+                    />
+                    <Stack.Screen
+                        options={{title: 'This is a repo'}}
+                        name="Repository"
+                        component={Repository}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </PaperProvider>
+    );
 };
 
 export default App;
